b82f4277888b60b4369549f784a1080ff10f6295
Jairo Diaz <jairoasd@hotmail.com>
2018-06-12 01:45:42 -0400

    Impements some project structure for routes and some basic authentication

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express');
+const apiRouter = require('./routes/api');
 const app = express();
 
 app.use(express.json());
 
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use('/api', apiRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
+
