35b4c7cec06fc03a7a9a80ad6f011f8cd786f3c7
Jairo Diaz <10743542+hdmontero@users.noreply.github.com>
2020-01-21 00:26:40 -0400

    Fixes config files

--- config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // bcrypt secret key
-    secretKey: '209f2b46055e9d9d44072e565ede9d4f',
-
-    // Google Places API key
-    GOOGLE_PLACES_KEY: 'AIzaSyBVAF6MqWkV5IGQ7LpRP8IJ2kPJgTZG-78'
-}
---
+++ config/config.sample.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // bcrypt secret key
+    secretKey: 'top-secret-random-string',
+
+    // Google Places API key
+    GOOGLE_PLACES_KEY: 'log-google-places-api-key-string'
+}
